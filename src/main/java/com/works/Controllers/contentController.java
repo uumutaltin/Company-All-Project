8310e43f42c34b30873f6a3daf25bca837e8418c
Umut Altın <umutaltin60@gmail.com>
2021-10-12 22:39:08 +0300

    Update

---
+++ src/main/java/com/works/Controllers/contentController.java
@@ -0,0 +1,23 @@
+package com.works.Controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("contents")
+public class contentController {
+
+    @GetMapping("")
+    public String content(){
+        return "contents";
+    }
+
+    @GetMapping("/add")
+    public String contentAdd(){
+        return "contentsAdd";
+    }
+
+
+
+}
