1432e784b0201bd19a852d3ed70293a1ce5f0c0d
Yusuf Efe <efeyusuf1999@gmail.com>
2021-10-14 19:22:32 +0300

    Login Page Completed

---
+++ src/main/java/com/works/Controllers/LoginController.java
@@ -0,0 +1,13 @@
+package com.works.Controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class LoginController {
+    @GetMapping("/login")
+    public String login(){
+        return "login";
+    }
+}
